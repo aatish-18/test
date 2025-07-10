45839d0eaa6b1611aeac0c4061639931c94cdd83
aatish <aatishjawalkar18@gmail.com>
2025-07-10 17:03:41 +0530

    first commit

---
+++ word_processor.py
@@ -0,0 +1,23 @@
+
+class WordVectorizer :
+    def __init__(self):
+        self.vocab =[]
+        self.token_dict = {}
+
+    def vocab_builder(self , file_path) :
+        with open(file= file_path) as file :
+            for lines in file.readlines() :
+                words = lines.strip()
+                self.vocab.append(words)
+
+    def printer(self):
+        print(self.vocab)
+
+
+
+first = WordVectorizer()
+first.vocab_builder("shakespeare.txt")
+first.printer()
+
+
+        
